f05e4415c806bb6119621beffa02ba668d807772
maxxHildebrand <mbhildebrand6@gmail.com>
2024-02-19 19:01:59 -0500

    Add react-icons package and update Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react"; // Import React
+import { CiLocationOn, CiSun } from "react-icons/ci";
 
 type Props = {}; // Define the Props type
 
@@ -9,7 +10,13 @@ export default function Navbar({} : Props) { // Define the Navbar component
         max-w-7x1 px-3 mx-auto">
             <p className="flex items-center justify-center gap-2">
                 <h2 className="text-grey-500 text-3x1">Weather</h2>
+                <CiSun className="text-3xl mt-1 text bg-yellow-300" />
             </p>
+
+            <section className="flex gap-2 items-center">
+              <CiLocationOn className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer" />
+
+            </section>
         </div>
         Navbar
     </nav>
